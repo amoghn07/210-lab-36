4d6a4143563a4e7163b8da12622c9b859f1acd1d
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-05-13 21:30:26 -0700

    Milestone 3

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -6,7 +6,18 @@
 using namespace std;
 
 int main() {
-    
+    ifstream fin;
+    string val;
+    fin.open("codes.txt");
+    //creating a BST
+    IntBinaryTree tree;
 
+    //populating tree with for loop
+    for (int i = 0; i < 10; i++){
+        fin >> val;
+        tree.insertNode(val);
+    }
+    tree.displayInOrder();
+    
     return 0;
 }
